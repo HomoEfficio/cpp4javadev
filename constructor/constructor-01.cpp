c74778639ff5ebd20daa9b2149ab90448dc91dea
Homo Efficio <homo.efficio@gmail.com>
2018-05-24 00:27:51 +0900

    ctor-initializer 방식 생성자

--- constructor/constructor-01.cpp
+++ constructor/constructor-01.cpp
@@ -4,20 +4,45 @@
 #include <iostream>
 using namespace std;
 
+class OtherTmp {
+private:
+    const int b;
+
+public:
+    OtherTmp(const int b) : b {b} {}
+
+    const int getB() const {
+        return b;
+    }
+};
+
 class Tmp {
 private:
     const int a;
+    OtherTmp& otherTmp;
 
 public:
-    Tmp(const int a) : a {a} {}
+    Tmp(const int a, OtherTmp& otherTmp) : a {a}, otherTmp {otherTmp} {}  // ctor-initializer 방식
+
+//    Tmp(const int a, OtherTmp& otherTmp) {
+//        this->a = a;  // const 변수에 할당을 시도하면 컴파일 에러
+//        this->otherTmp = otherTmp;
+//    }
 
     const int getA() const {
         return a;
     }
+
+    OtherTmp &getOtherTmp() const {
+        return otherTmp;
+    }
 };
 
 int main() {
-    Tmp tmp1 {1};
+    OtherTmp otherTmp {3};
+    OtherTmp& otherTmpRef = otherTmp;
+    Tmp tmp1 {1, otherTmpRef};
 
     cout << tmp1.getA() << endl;
+    cout << tmp1.getOtherTmp().getB() << endl;
 }
